9cc40bffb555acad2168c595c9fa30cbfe8948e7
Shamar Morrison <shamar.webdev@gmail.com>
2025-10-20 12:38:48 -0500

    Update Album model to replace description with artist reference, enhancing relationships with Artist model.

--- src/models/album.model.ts
+++ src/models/album.model.ts
@@ -1,11 +1,12 @@
 import { prop, getModelForClass, type Ref } from "@typegoose/typegoose";
+import type { Artist } from "models/artists.model.js";
 
 export class Album {
   @prop({ required: [true, "Album Title is required"], trim: true })
   title!: string;
 
   @prop({ required: true, trim: true })
-  description!: string;
+  artist!: Ref<Artist>;
 
   @prop({ required: true, trim: true })
   image!: string;
