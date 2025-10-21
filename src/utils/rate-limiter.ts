c927440861bdd719d150addada5170a98532db3b
Shamar Morrison <shamar.webdev@gmail.com>
2025-10-20 21:46:23 -0500

    Refactor rate limiter error response to use http-status-codes for improved readability and maintainability

--- src/utils/rate-limiter.ts
+++ src/utils/rate-limiter.ts
@@ -1,10 +1,11 @@
 import { rateLimit } from "express-rate-limit";
+import { StatusCodes } from "http-status-codes";
 
 export const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   handler: (_, res) => {
-    res.status(429).json({
+    res.status(StatusCodes.TOO_MANY_REQUESTS).json({
       message: "Too many requests. Please try again later.",
     });
   },
