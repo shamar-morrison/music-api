76af8c83d91bc102e9f671d66501d3598f6889da
Shamar Morrison <shamar.webdev@gmail.com>
2025-10-20 21:45:27 -0500

    Enhance rate limiter configuration by adding a comment to clarify request limit per window

--- src/utils/rate-limiter.ts
+++ src/utils/rate-limiter.ts
@@ -2,7 +2,7 @@ import { rateLimit } from "express-rate-limit";
 
 export const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 100,
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   handler: (_, res) => {
     res.status(429).json({
       message: "Too many requests. Please try again later.",
